41c4484cc0688a48b7148c50d241c23ef7b519dc
Megi Chkhetia <mchkhetia@my.wctc.edu>
2025-02-23 09:33:29 -0600

    registered my gutenberg-block in php file

--- high-pulp-blocks.php
+++ high-pulp-blocks.php
@@ -27,6 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 function mc_high_pulp_blocks_block_init() {
 	register_block_type( __DIR__ . '/build/blocks/high-pulp-blocks' );
 	register_block_type( __DIR__ . '/build/blocks/testimonial' );
+	register_block_type( __DIR__ . '/build/blocks/gutenberg-block' );
 
 }
 add_action( 'init', 'mc_high_pulp_blocks_block_init' );
