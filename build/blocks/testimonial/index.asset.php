2b6860e5c26167aa0e65fc4eb91178c6804af525
Megi Chkhetia <mchkhetia@my.wctc.edu>
2025-02-24 10:04:57 -0600

    create color picker and color palette

--- build/blocks/testimonial/index.asset.php
+++ build/blocks/testimonial/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '90deef21c7f9e15d3aee');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8b217f3fa13fdb7ec180');
