c5b1181f8f5d11eaa1bc5be849b51073de3fe103
Megi Chkhetia <mchkhetia@my.wctc.edu>
2025-03-06 09:53:35 -0600

    craeted StaffList.js

--- src/blocks/staff-directory/view.js
+++ src/blocks/staff-directory/view.js
@@ -1,25 +1,12 @@
-/**
- * Use this file for JavaScript code that you want to run in the front-end 
- * on posts/pages that contain this block.
- *
- * When this file is defined as the value of the `viewScript` property
- * in `block.json` it will be enqueued on the front end of the site.
- *
- * Example:
- *
- * ```js
- * {
- *   "viewScript": "file:./view.js"
- * }
- * ```
- *
- * If you're not making any changes to this file because your project doesn't need any 
- * JavaScript running in the front-end, then you should delete this file and remove 
- * the `viewScript` property from `block.json`. 
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
- */
- 
-/* eslint-disable no-console */
-console.log("Hello World! (from mc-staff-directory block)");
-/* eslint-enable no-console */
+import { createRoot } from 'react-dom/client';
+import BlockApp from "./react-app/BlockApp";
+
+
+const blocks = document.querySelectorAll('.wp-block-mc-staff-directory')
+
+
+// Render your React component instead
+blocks.forEach(block => {
+	createRoot(block).render(<BlockApp/>)
+})
+
