717b0260ac0cb085af3ecfe893bb8f01c9625d19
Megi Chkhetia <mchkhetia@my.wctc.edu>
2025-03-06 09:29:35 -0600

    created staff directory block

---
+++ build/blocks/staff-directory/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b6920b355903d1468e8e');
--- high-pulp-blocks.php
+++ high-pulp-blocks.php
@@ -29,6 +29,7 @@ function mc_high_pulp_blocks_block_init() {
 	register_block_type( __DIR__ . '/build/blocks/testimonial' );
 	register_block_type( __DIR__ . '/build/filters/border-control' );
 	register_block_type( __DIR__ . '/build/blocks/staff-list' );
+	register_block_type( __DIR__ . '/build/blocks/staff-directory' );
 
 }
 add_action( 'init', 'mc_high_pulp_blocks_block_init' );
