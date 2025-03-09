56321d83f5f57b4d867efec7f45cc7ea79a0eccd
Megi Chkhetia <mchkhetia@my.wctc.edu>
2025-03-09 13:11:19 -0500

    added into render.php and added some styling, woot woot, its kinda showing, heheheh

--- build/blocks/book-block/index.asset.php
+++ build/blocks/book-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cebe54e6abce8e028992');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9bc749499a894f26a941');
